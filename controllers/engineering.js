f05b8e410970b2a71e5d2ea4571d9c985814b202
MehalS542312 <77812392+MehalS542312@users.noreply.github.com>
2021-04-13 21:02:23 -0500

    create

--- controllers/engineering.js
+++ controllers/engineering.js
@@ -99,3 +99,18 @@ exports.engineering_view_one_Page = async function(req, res) {
     }
 };
 
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.engineering_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('engineeringcreate', { title: 'Engineering Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
--- routes/engineering.js
+++ routes/engineering.js
@@ -6,3 +6,5 @@ router.get('/', engineering_controlers.engineering_view_all_Page );
 module.exports = router;
 /* GET detail costume page */
 router.get('/detail', engineering_controlers.engineering_view_one_Page);
+/* GET create costume page */
+router.get('/create', engineering_controlers.engineering_create_Page);
